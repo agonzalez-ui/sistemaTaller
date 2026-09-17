ae8713e6fb187f223bf9791317f1a018d84c707e
agonzalez-ui <agonzalezu@outlook.es>
2026-09-17 03:10:26 -0600

    agrego nuevos controladores

---
+++ app/Http/Controllers/CustomerPhoneController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomerPhone;
+use Illuminate\Http\Request;
+
+class CustomerPhoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerPhone $customerPhone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerPhone $customerPhone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomerPhone $customerPhone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerPhone $customerPhone)
+    {
+        //
+    }
+}
